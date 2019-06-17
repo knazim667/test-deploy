29568a142c3d6789914b3fc982e883e793df3539
Muhammad Nazam <muhammadnazam@macbook-pro-2.home>
2019-06-16 21:00:03 -0700

    test deploy

---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+
+module.exports = function(req, res, next) {
+
+    // get token
+    const token = req.header('x-auth-token');
+    // check if there is no token
+    if(!token) {
+        return res.status(401).json({ msg: 'no Token , authorized denied'});
+
+    }
+
+    // verify token
+    try{
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+        req.user = decoded.user;
+        next();
+    }catch(err){
+        return res.status(401).json({ msg: 'Token is not valid'});
+    }
+}
